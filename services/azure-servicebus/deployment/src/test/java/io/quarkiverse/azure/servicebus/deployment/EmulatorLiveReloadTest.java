5371a18d3e56c1994b4b995262356725194acae9
Harald Albers <harald.albers@hamburger-software.de>
2025-11-27 12:46:29 +0100

    The config file is copied but not found by the producer.

--- services/azure-servicebus/deployment/src/test/java/io/quarkiverse/azure/servicebus/deployment/EmulatorLiveReloadTest.java
+++ services/azure-servicebus/deployment/src/test/java/io/quarkiverse/azure/servicebus/deployment/EmulatorLiveReloadTest.java
@@ -1,28 +1,29 @@
 package io.quarkiverse.azure.servicebus.deployment;
 
-import io.quarkus.test.QuarkusDevModeTest;
-import io.restassured.RestAssured;
-import org.jboss.logmanager.Level;
+import static io.restassured.RestAssured.when;
+
 import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
-import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.when;
+import io.quarkus.test.QuarkusDevModeTest;
+import io.restassured.RestAssured;
 
 public class EmulatorLiveReloadTest {
 
     @RegisterExtension
-    static final QuarkusDevModeTest DEV_MODE_TEST = new QuarkusDevModeTest()
-            .withApplicationRoot((jar) -> jar
+    static final QuarkusDevModeTest TEST = new QuarkusDevModeTest()
+            .withApplicationRoot(jar -> jar
                     .addClass(ReloadResource.class)
                     .addAsResource(
                             new StringAsset(
                                     """
                                             quarkus.azure.servicebus.devservices.license-accepted=true
+                                            quarkus.azure.servicebus.devservices.emulator.config-file-path=my-custom-config.json
                                             """),
-                            "application.properties"));
+                            "application.properties"))
+            .setCodeGenSources("azure");
 
     @BeforeAll
     static void setUp() {
